59ac7f89c9a446ae17b1d13e3ee6c6709940c583
Víctor Pérez Campos <126863457+victorperezcampos@users.noreply.github.com>
2023-05-17 12:13:01 +0200

    Create Individual12.c

---
+++ TrabajoIndividual12/Individual12.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <string.h>
+
+#define N 50
+
+typedef char cadena50[N+1];
+
+void invertir (cadena50 cad1, cadena50 cad2);
+
+int main ()
+{
+    cadena50 cad1, cad2;
+
+    printf ("\nIntroduce cadena de caracteres: ");
+    gets (cad1);
+    invertir (cad1, cad2); 
+    printf ("\nLa cadena invertida es: %s", cad2);
+}
+
+void invertir (cadena50 cad1, cadena50 cad2)
+{
+    int i, j;
+
+    j = 0;
+    for (i=strlen(cad1)-1; i>=0; i--)
+    {
+        cad2[j] = cad1[i];
+        j++;
+    }
+    cad2[j] = '\0';
+}
+
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define N 50
+typedef char cadena50[N+1];
+
+void dejaralfabeticos (cadena50 cad, int *longitud);
+
+int main ()
+{
+    cadena50 cad;
+    int longitud;
+
+    printf ("\nIntroduce cadena: ");
+    gets (cad);
+    longitud = strlen(cad);
+    dejaralfabeticos (cad, &longitud);
+    printf ("\nLa cadena solo alfabetica es: %s", cad);
+}
+
+void dejaralfabeticos (cadena50 cad, int *longitud)
+{
+    int i, j;
+
+    i = 0;
+    while (i < *longitud)
+    {
+        if (isalpha (cad[i]))
+            i++;
+        else
+        {
+            for (j=i; j<*longitud; j++)
+                cad[j] = cad[j+1];
+            (*longitud)--;
+        }
+    }
+}
+
+#include <stdio.h>
+#include <conio.h>
+
+#define N 50
+typedef char cadena50[N+1];
+
+void leer_cadena(cadena50 cad);
+void imprimirpalabras (cadena50 cad);
+int longitudcadena (cadena50 cad);
+
+int main ()
+{
+    cadena50 cad;
+
+    printf ("\nIntroduce cadena de como maximo %d caracteres (ENTER para terminar):\n", N);
+    leer_cadena (cad);
+    imprimirpalabras (cad);
+}
+
+void leer_cadena(cadena50 cad)
+{
+    int i,fin;
+    char c;
+
+    i=0;
+    fin=0;
+    while((i<N)&&(!fin))
+    {
+        c=getch();
+        switch(c){
+        case '\r': fin=1;
+                   printf("\n");
+                   break;
+        case '\b': if (i>0){
+                     --i;
+                     printf("\b \b");
+                   }
+                   break;
+        default:   cad[i]=c;
+                   ++i;
+                   printf("%c",c);
+                   break;
+        }
+    }
+    cad[i]='\0';
+    if (i==N)
+        printf("\n");
+}
+
+void imprimirpalabras (cadena50 cad)
+{
+    int n, i, j;
+    cadena50 palabra;
+
+    printf ("\nLas palabras son:\n");
+    n = longitudcadena (cad);
+    i = 0;
+    while (i < n)
+    {
+        j = 0;
+        while (i<n && cad[i]!=' ')
+        {
+            palabra[j] = cad[i];
+            i++;
+            j++;
+        }
+        palabra[j] = '\0';
+        dejaralfabeticos(palabra);
+        if (longitudcadena(palabra) != 0)
+            printf ("\n%s", palabra);
+        i++;
+    }
+}
+
+int longitudcadena (cadena50 cad)
+{
+    int i;
+
+    i = 0;
+    while (cad[i] != '\0')
+        i++;
+    return i;
+}
+
+
+void dejaralfabeticos (cadena50 cad)
+{
+    int i, j, n;
+
+    i = 0;
+    n = longitudcadena (cad);
+    while (i < n)
+    {
+        if ((cad[i]>='a' && cad[i]<='z') ||
+                (cad[i]>='A' && cad[i]<='Z'))
+            i++;
+        else
+        {
+            for (j=i; j<n; j++)
+                cad[j] = cad[j+1];
+            (n)--;
+        }
+    }
+}
